82c2f7182a0e4e98f0e7fc1c7ef1e329abc356df
Admin <amin@admin.com>
2024-07-18 12:35:55 +0300

    Добавлены тесты для ExpressionCalculator

---
+++ StringExpressionCalculator.Tests/ExpressionCalculatorIntegrationTests.cs
@@ -0,0 +1,49 @@
+﻿using StringExpressionCalculator.Domain.Application.Operations;
+using StringExpressionCalculator.Domain.Application.Operations.Abstractions;
+using StringExpressionCalculator.Domain.Application.Services.Abstractions;
+using StringExpressionCalculator.Domain.Application.Services;
+
+namespace StringExpressionCalculator.Tests;
+
+public class ExpressionCalculatorIntegrationTests : IClassFixture<CalculatorDataFixture>
+{
+    private readonly IExpressionCalculator _target;
+
+    public ExpressionCalculatorIntegrationTests(CalculatorDataFixture calculatorData) 
+        => _target = calculatorData.Target;
+
+    [Theory]
+    [InlineData("1 + 2 - 4 / (1 - 2)", 7)]
+    [InlineData("(-2 * 4.5 - (24 / 2 ^ 4)) + 4!", 13.5)]
+    [InlineData("(-3.14 - (-8.25) + (3.5 ^ 2 / 5 + 3!)) - 3.14", 10.42)]
+    public void CalculateTests(string expression, double expectedResult)
+    {
+        var result = double.Round(_target.Calculate(expression), 3);
+        Assert.Equal(expectedResult, result);
+    }
+}
+
+public class CalculatorDataFixture
+{
+    public IExpressionCalculator Target { get; }
+
+    public CalculatorDataFixture()
+    {
+        IMathOperation[] operations =
+        [
+            new SumOperation(), 
+            new SubtractionOperation(), 
+            new MultiplicationOperation(), 
+            new DivisionOperation(),
+            new ExponentiationOperation(),
+            new FactorialOperation()
+        ];
+
+        var provider = new OperationsProvider(operations);
+        var calculator = new Calculator(provider);
+        var stringParser = new StringParser(provider);
+        var expressionParser = new ExpressionParser(stringParser);
+
+        Target = new ExpressionCalculator(expressionParser, calculator);
+    }
+}
---
+++ StringExpressionCalculator.Tests/ExpressionCalculatorTests.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using StringExpressionCalculator.Domain.Application.Services;
+using StringExpressionCalculator.Domain.Application.Services.Abstractions;
+using StringExpressionCalculator.Domain.Domain;
+
+namespace StringExpressionCalculator.Tests;
+
+public class ExpressionCalculatorTests
+{
+    [Fact]
+    public void CalculateTest()
+    {
+        // Arrange
+        const string exp = "1 + 2 - 4 / (1 - 2)";
+
+        var r1 = new BinaryOperationContext(new Operand(1), new Operand(2), "-");
+        var r2 = new BinaryOperationContext(new Operand(4), new Operand(r1), "/");
+        var r3 = new BinaryOperationContext(new Operand(1), new Operand(2), "+");
+        var r4 = new BinaryOperationContext(new Operand(r3), new Operand(r2), "-");
+        var parserMock = Mock.Of<IExpressionParser>(x => x.Parse(exp) == new OperationContext[] { r1, r2, r3, r4 });
+
+        var calculatorMock = new Mock<ICalculator>();
+        calculatorMock.SetupSequence(x => x.CalculateBinary(It.IsAny<double>(), It.IsAny<string>(), It.IsAny<double>()))
+            .Returns(-1).Returns(-4).Returns(3).Returns(7);
+
+        var target = new ExpressionCalculator(parserMock, calculatorMock.Object);
+
+        // Act
+        var result = target.Calculate(exp);
+
+        // Asserts
+        Assert.Equal(7, result);
+    }
+}
